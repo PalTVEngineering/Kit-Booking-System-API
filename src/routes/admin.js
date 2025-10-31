e19f8381be16350a5bcdb3376881a3044c04bed3
shawn <lanzy@saanw.com>
2025-10-31 22:00:56 +0000

    feat(admin): admin booking list

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { adminLogin } from "../controllers/admin.js";
+import { adminLogin, getAllBookingsWithKits } from "../controllers/admin.js";
 
 const router = express.Router();
 
 // POST /api/admin/login
 router.post("/login", adminLogin);
+router.get("/bookings", getAllBookingsWithKits);
 
 export default router;
